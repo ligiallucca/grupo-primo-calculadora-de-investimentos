cd6f7b6dbd3276d3f12ef143bfb02853dbd290ae
ligiallucca <lilillucca@gmail.com>
2024-01-18 18:56:39 -0300

    adicionando sass, variavéis,  mixins e responsividade do header

--- src/components/GPHeader.vue
+++ src/components/GPHeader.vue
@@ -5,15 +5,25 @@
     </h1>
   </header>
 </template>
-<style scoped>
+<style lang="scss" scoped>
+@import './../assets/main.scss';
 header {
   width: 100%;
   display: flex;
   padding: 32px 120px;
   justify-content: start;
+  @include for-small-devices {
+    padding: 32px;
+  }
+  img {
+    width: 176px;
+    height: 32px;
+    @include for-small-devices {
+      width: 120px;
+      height: 22px;
+    }
+  }
+
 }
-img {
-  width: 176px;
-  height: 32px;
-}
+
 </style>
